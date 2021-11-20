318164ec91a64fc8ec8a2826aed3839c9ec380cf
Paul Mason <webdevpaulmason@gmail.com>
2021-11-20 15:48:31 -0800

    clear use state

--- src/components/CondensedRecipe.jsx
+++ src/components/CondensedRecipe.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Button, CircularProgress, Grid, Typography } from "@material-ui/core";
 import { useSelector, useDispatch } from "react-redux";
 import {
